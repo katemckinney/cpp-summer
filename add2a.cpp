d3ceed3dfc6ec0d82f97645e9ecc343fb2616f14
kmckin19 <kmckin19@hills.ccsf.edu>
2025-06-06 18:21:18 -0700

    Add C++ source and executable files for demo

---
+++ add2a.cpp
@@ -0,0 +1,7 @@
+//Function add
+int add( int num1, int num2 )
+{
+
+  return num1 + num2 ; 
+
+}
---
+++ hello2a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+using namespace std ;
+
+//Function add
+int add( int num1, int num2 )
+{
+
+  return num1 + num2 ;
+
+}
+
+
+int main()
+
+{
+
+   int ret = add( 3 , 4 ) ;
+
+   cout << ret << endl ;
+
+   return(0) ;
+
+}
+
