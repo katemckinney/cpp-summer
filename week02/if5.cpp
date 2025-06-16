0bb6be180400c2ec311449f54d70434565861f0e
katemckinney <kate.m.mckinney@gmail.com>
2025-06-15 22:41:31 -0700

    add if example that further clarifies boolean logic

---
+++ week02/if5.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+        int x1 = 5, y1 = 15;
+        if ( x1 = 6 ) // assignment, not comparison; assigns 6 to x1, then evaluates to true since 6 ≠ 0
+            cout << "If statement." << endl;
+        return 0;
+}
